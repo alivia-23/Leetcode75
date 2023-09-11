09f9d056bc7e5924dd0b7e9419e360551f9e9823
alivia-23 <guin.al@husky.neu.edu>
2023-09-10 23:47:04 -0700

    edge cases

--- src/Array/IncreasingTriplet.java
+++ src/Array/IncreasingTriplet.java
@@ -21,6 +21,8 @@ package Array;
 
 public class IncreasingTriplet {
     public static boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) return false;
+        
         int firstNum = Integer.MAX_VALUE;
         int secondNum = Integer.MAX_VALUE;
 
